ff07b32759e00740d172dc1234bc7369cac22a4b
Mayur0101 <149255660+Mayur0101@users.noreply.github.com>
2023-10-29 13:40:12 +0530

    Add files via upload

---
+++ CriatoSoft_Website/CriatoSoft_Website/src/main/java/com/criatosoft/main/CriatoSoft_Website/CriatoSoftWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.criatosoft.main.CriatoSoft_Website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CriatoSoftWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CriatoSoftWebsiteApplication.class, args);
+	}
+
+}
---
+++ CriatoSoft_Website/CriatoSoft_Website/src/test/java/com/criatosoft/main/CriatoSoft_Website/CriatoSoftWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.criatosoft.main.CriatoSoft_Website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriatoSoftWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
